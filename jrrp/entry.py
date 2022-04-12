d12e43e89e6eca6614c91498b5934caf9d2f4019
Huaji_MUR233 <huajimur@jvav.me>
2022-04-12 10:28:59 +0800

    better config

--- jrrp/entry.py
+++ jrrp/entry.py
@@ -1,14 +1,11 @@
 from datetime import datetime
+import os.path
 
 from mcdreforged.api.types import PluginServerInterface
 from mcdreforged.api.command import Literal
+from pydantic import Protocol
 
-
-DEFAULT_CONFIG = {
-    "enable": True,
-    "online_mode": True,
-    "command": "!!jrrp"
-}
+from .config import JrrpConfig
 
 
 def get_jrrp(string: str):
@@ -18,38 +15,27 @@ def get_jrrp(string: str):
     return num2
 
 
-def get_jrrp_msg(uuid: str):
-    jrrp = get_jrrp(uuid)
-    if jrrp == 100:
-        msg = "！100！100！！！！！"
-    elif jrrp == 99:
-        msg = "！但不是 100……"
-    elif jrrp >= 90:
-        msg = "！好评如潮！"
-    elif jrrp >= 60:
-        msg = "！是不错的一天呢！"
-    elif jrrp >= 50:
-        msg = "！还行啦还行啦。"
-    elif jrrp == 50:
-        msg = "！五五开……"
-    elif jrrp >= 40:
-        msg = "！还……还行吧……？"
-    elif jrrp >= 11:
-        msg = "！呜哇……"
-    elif jrrp >= 1:
-        msg = "……（没错，是百分制）"
-    else:
-        msg = "……"
-    return "你今天的人品值是：" + str(jrrp) + msg
+def register_jrrp_command(server: PluginServerInterface):
+    def get_jrrp_(player):
+        uuid = mc_uuid.onlineUUID(player).hex if config.online_mode else mc_uuid.offlineUUID(player).hex
+        jrrp = get_jrrp(uuid)
+        for msg_obj in config.message:
+            if eval(msg_obj["expr"], {}, {"jrrp": jrrp}):
+                start = msg_obj["start"] if msg_obj["start"] else config.start
+                end = msg_obj["end"] if msg_obj["end"] else config.end
+                return start + str(jrrp) + end
+
+    config = server.load_config_simple(os.path.join("config", "jrrp.json"),
+                                       in_data_folder=False,
+                                       target_class=JrrpConfig)
+    mc_uuid = server.get_plugin_instance("mc_uuid")
+    for command in config.command:
+        server.register_command(
+            Literal(command)
+            .requires(lambda src: src.is_player)
+            .runs(lambda src: src.reply(get_jrrp_(src.player)))
+        )
 
 
 def on_load(server: PluginServerInterface, old):
-    config = server.load_config_simple(default_config=DEFAULT_CONFIG)
-    mc_uuid = server.get_plugin_instance("mc_uuid")
-    if config["enable"]:
-        server.register_command(
-            Literal(config["command"])
-            .requires(lambda src: src.is_player)
-            .runs(lambda src: src.reply(get_jrrp_msg(mc_uuid.onlineUUID(src.player).hex if config["online_mode"] else mc_uuid.offlineUUID(src.player).hex)))
-        )
-        server.register_help_message(config["command"], "今日人品")
+    register_jrrp_command(server)
